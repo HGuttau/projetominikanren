c4b2514a890fbe6aa05a4300d0dc13ab63455763
HGuttau <workguttau@gmail.com>
2025-03-23 20:33:42 -0300

    adicionado irmaos ao praticando a2

--- gitminikanren/praticandoA2.py
+++ gitminikanren/praticandoA2.py
@@ -3,8 +3,14 @@ from kanren.constraints import neq
 
 mae = Relation()
 pai = Relation()
+
 facts(mae, ('Carol', 'John'))
+facts(mae, ('Carol', 'Julia'))
+facts(mae, ('Carol', 'Leo'))
+
 facts(pai, ('Eric', 'John'))
+facts(pai, ('Eric', 'Julia'))
+facts(pai, ('Eric', 'Leo'))
 
 def pais(x, pessoa):
    return lany(
@@ -12,5 +18,22 @@ def pais(x, pessoa):
        pai(x, pessoa),
    )
 
+def filhos(pessoa, x):
+   return lany(
+      mae(pessoa, x),
+      pai(pessoa, x)
+   )
+
+
+def irmaos(pessoa, x):
+    paisemcomum = var()
+    return lall(
+       pais(paisemcomum, pessoa),
+       pais(paisemcomum, x),
+       neq(pessoa, x)
+    )
+   
+
+
 x = var()
-print(run(2, x, pais(x, 'John')))
+print(run(2, x, irmaos('John', x)))
